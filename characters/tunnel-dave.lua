b2b7f79029088b134c22cfced405a8527f088328
Junior <87715171+JuniorNovoa1@users.noreply.github.com>
2023-01-21 16:19:45 -0500

    Update tunnel-dave.lua

--- characters/tunnel-dave.lua
+++ characters/tunnel-dave.lua
@@ -1,9 +1,10 @@
 local originalPos = 0;
 local speedAmount = 4.5;
+local tweenType = 'sineInOut';
 
 function onSongStart()
     originalPos = getProperty('dad.y')
-    doTweenY('daveDown', 'dad', originalPos +125, speedAmount / 2)
+    doTweenY('daveDown', 'dad', originalPos +125, speedAmount / 2, tweenType)
 end
 
 function onUpdate()
@@ -15,10 +16,10 @@ end
 
 function onTweenCompleted(tag)
     if tag == 'daveDown' then
-        doTweenY('daveUp', 'dad', originalPos -300, speedAmount + 1.25)
+        doTweenY('daveUp', 'dad', originalPos -300, speedAmount + 1.25, tweenType)
     end
 
     if tag == 'daveUp' then
-        doTweenY('daveDown', 'dad', originalPos +125, speedAmount)
+        doTweenY('daveDown', 'dad', originalPos +125, speedAmount, tweenType)
     end
 end
