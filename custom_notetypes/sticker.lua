585f18328da46a602b7311a54fcd182fdd99acef
Junior <87715171+JuniorNovoa1@users.noreply.github.com>
2023-06-26 13:39:33 -0400

    Algebra is finished

---
+++ custom_notetypes/sticker.lua
@@ -0,0 +1,69 @@
+function onCreatePost()
+	--Iterate over all notes
+	for i = 0, getProperty('unspawnNotes.length')-1 do
+		--Check if the note is an Instakill Note
+		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'sticker' then
+			setPropertyFromGroup('unspawnNotes', i, 'texture', 'STICKERassets'); --Change texture
+			setPropertyFromGroup('unspawnNotes', i, 'hitHealth', '0.023'); --Default value is: 0.023, health gained on hit
+			setPropertyFromGroup('unspawnNotes', i, 'missHealth', '0.0475'); --Default value is: 0.0475, health lost on miss
+			if getPropertyFromGroup('unspawnNotes', i, 'noteData') == 0 then
+				setPropertyFromGroup('unspawnNotes', i, 'offset.x', 85); --offset
+			elseif getPropertyFromGroup('unspawnNotes', i, 'noteData') == 1 then
+				setPropertyFromGroup('unspawnNotes', i, 'offset.x', 80); --offset
+			elseif getPropertyFromGroup('unspawnNotes', i, 'noteData') == 2 then
+				setPropertyFromGroup('unspawnNotes', i, 'offset.x', 65); --offset
+			elseif getPropertyFromGroup('unspawnNotes', i, 'noteData') == 3 then
+				setPropertyFromGroup('unspawnNotes', i, 'offset.x', 75); --offset
+			end
+			setPropertyFromGroup('unspawnNotes', i, 'offset.y', 0); --offset
+			setPropertyFromGroup('unspawnNotes', i, 'hitCausesMiss', false);
+
+			if getPropertyFromGroup('unspawnNotes', i, 'mustPress') then --Doesn't let Dad/Opponent notes get ignored
+				setPropertyFromGroup('unspawnNotes', i, 'ignoreNote', true); --Miss has no penalties
+			end
+		end
+	end
+	--debugPrint('Script started!')
+end
+
+-- Function called when you hit a note (after note hit calculations)
+-- id: The note member id, you can get whatever variable you want from this note, example: "getPropertyFromGroup('notes', id, 'strumTime')"
+-- noteData: 0 = Left, 1 = Down, 2 = Up, 3 = Right
+-- noteType: The note type string/tag
+-- isSustainNote: If it's a hold note, can be either true or false
+local scale = 0.8;
+local timer = 0.2;
+local minAmount = 20;
+local maxAmount = 20;
+function goodNoteHit(id, noteData, noteType, isSustainNote)
+	if getPropertyFromGroup("notes", id, 'noteType') ~= 'sticker' then return; end
+	for i = 1, getRandomInt(minAmount, maxAmount) do
+		makeLuaSprite("Sticker"..i, "RECOVER_assets/stickers/"..getRandomInt(1, 32), getRandomInt(-200, screenWidth / 1.35), getRandomInt(-200, screenHeight / 1.35))
+		setObjectCamera("Sticker"..i, "other")
+		setProperty("Sticker"..i..".alpha", 0)
+		addLuaSprite("Sticker"..i)
+		doTweenAlpha("StickerAA"..i, "Sticker"..i, 1, timer, "")
+		doTweenX("StickerX"..i, "Sticker"..i..".scale", scale + 0.2, timer, "")
+		doTweenY("StickerY"..i, "Sticker"..i..".scale", scale + 0.2, timer, "")
+		runTimer("stickers", 6, 1)
+	end
+end
+
+function onTweenCompleted(tag)
+	for i = 1, maxAmount do
+		if tag == 'StickerY'..i then
+			doTweenY("StickerY"..i, "Sticker"..i..".scale", scale, timer, "")
+		end
+		if tag == 'StickerA'..i then
+			removeLuaSprite("Sticker"..i, true)
+		end
+	end
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+	if tag == "stickers" then
+		for i = 1, maxAmount do
+			doTweenAlpha("StickerA"..i, "Sticker"..i, 0, timer, "")
+		end
+	end
+end
--- data/algebra/script.lua
+++ data/algebra/script.lua
@@ -1,14 +1,57 @@
 local originalSpeed = 3;
 local swagSpeed = 1.6;
 
+function onCreatePost()
+	makeLuaSprite('thunderBlack', '', getProperty("bg.x"), getProperty("bg.y"))
+    makeGraphic('thunderBlack', getProperty("bg.width"), getProperty("bg.height"), '000000')
+    setProperty('thunderBlack.antialiasing', getPropertyFromClass('ClientPrefs', 'globalAntialiasing'))
+    setProperty('thunderBlack.alpha', 0)
+    addLuaSprite('thunderBlack', true)
+end
+
+local lockCam = false;
+local camZoomA = 1.25;
+
 function onUpdate()
-	setProperty("SONG.speed", swagSpeed)
+	if lockCam then
+        setDataFromSave('settings', 'camZoom', false)
+        doTweenZoom('camZoomGame', 'camGame', camZoomA, 1, "")
+    else
+        setDataFromSave('settings', 'camZoom', true)
+    end
+
+	runHaxeCode([[game.set_songSpeed(]]..swagSpeed..[[);]])
 end
 
 function onBeatHit()
+	if curBeat == 8 or curBeat == 72 or curBeat == 104 or curBeat == 152 or curBeat == 224 or curBeat == 256 or curBeat == 288 or curBeat == 320 or curBeat == 416 or curBeat == 472 or curBeat == 504 or curBeat == 544 
+	or curBeat == 576 or curBeat == 608 or curBeat == 640 or curBeat == 672 or curBeat == 704 or curBeat == 736 or curBeat == 740 or curBeat == 772 or curBeat == 804 or curBeat == 860 or curBeat == 907 or curBeat == 923 
+	or curBeat == 931 or curBeat == 935 or curBeat == 939 or curBeat == 942 or curBeat == 1008 or curBeat == 1072 or curBeat == 1168 or curBeat == 1232 or curBeat == 1295 and flashingLights then
+		cameraFlash('other', 'FFFFFF', 1.5) --cam flash simulator
+	end
+
 	if curBeat == 160 then
 		swagSpeed = originalSpeed - 0.5;
 		setProperty("daveJunk.visible", true)
+	end
+
+	if curBeat == 224 then
+		camZoomA = getProperty("defaultCamZoom") + 0.3;
+		lockCam = true;
+	end
+
+	if curBeat == 256 then
+		lockCam = false;
+	end
+
+	if curBeat == 288 then
+		lockCam = true;
+		doTweenAlpha("thunderBlack", "thunderBlack", 0.35, crochet / 500, "")
+	end
+
+	if curBeat == 320 then
+		lockCam = false;
+		doTweenAlpha("thunderBlack", "thunderBlack", 0, crochet / 500, "")
 	end
 
 	if curBeat == 352 then
@@ -22,9 +65,27 @@ function onBeatHit()
 		setProperty("davePiss.visible", true)
 	end
 
+	if curBeat == 436 then
+		lockCam = true;
+	end
+
+	if curBeat == 439 then
+		lockCam = false;
+	end
+
 	if curBeat == 440 then
 		setProperty("spike.visible", true)
 		setProperty("davePiss.visible", false)
+	end
+
+	if curBeat == 472 then
+		camZoomA = getProperty("defaultCamZoom") + 0.1;
+		lockCam = true;
+	end
+
+	if curBeat == 504 then
+		setProperty("defaultCamZoom", getProperty("defaultCamZoom") + 0.15)
+        lockCam = false;
 	end
 
 	if curBeat == 512 then
@@ -33,10 +94,41 @@ function onBeatHit()
 		swagSpeed = 2;
 	end
 
+	if curBeat == 672 then
+		setProperty("defaultCamZoom", getProperty("defaultCamZoom") + 0.05)
+	end
+
+	if curBeat == 740 then
+		setProperty("defaultCamZoom", getProperty("defaultCamZoom") - 0.05)
+	end
+
 	if curBeat == 836 then
 		setProperty("diamondJunk.visible", false)
 		setProperty("monitorJunk.visible", true)
 		swagSpeed = originalSpeed - 0.5;
+	end
+
+	if curBeat == 862 then
+		setProperty("defaultCamZoom", getProperty("defaultCamZoom") + 0.05)
+	end
+
+	if curBeat == 876 then
+		setProperty("defaultCamZoom", getProperty("defaultCamZoom") - 0.05)
+	end
+
+	if curBeat == 942 then
+		camZoomA = getProperty("defaultCamZoom") + 0.4;
+		lockCam = true;
+	end
+
+	if curBeat == 943 then
+		lockCam = false;
+	end
+
+	if curBeat == 1008 then
+		camZoomA = 0.9;
+		lockCam = true;
+		setProperty("defaultCamZoom", 1)
 	end
 
 	if curBeat == 1344 then
@@ -45,9 +137,13 @@ function onBeatHit()
 	end
 
 	if curBeat == 1136 then
-		makeLuaSprite('diamondJunk', 'main/algebra/bgJunkers/Crafters_Ohno', 645 + 75, -16);
-		addLuaSprite('diamondJunk', false);
+		--[[makeLuaSprite('diamondJunk', 'main/algebra/bgJunkers/Crafters_Ohno', 645 + 75, -16);
+		setProperty("diamondJunk.scale.x", 0.6)
+		setProperty("diamondJunk.scale.y", 0.6)
+		addLuaSprite('diamondJunk', false);--]]
 		setProperty("diamondJunk.visible", true)
+		lockCam = false;
+		setProperty("defaultCamZoom", 1.1)
 		swagSpeed = 1.6;
 	end
 
@@ -60,17 +156,20 @@ function onBeatHit()
 		swagSpeed = originalSpeed;
 	end
 	if curBeat == 1436 then
+		setProperty("monitorJunk.visible", false)
+		setProperty("diamondJunk.visible", false)
 		setProperty("davePiss.visible", false)
-		setProperty("robotJunk.visible", true)
+		setProperty("robotJunk.visible", false)
 	end
 	if curBeat == 1588 then
 		swagSpeed = originalSpeed + 0.9;
 	end
 end
 
-function goodNoteHit(membersIndex, noteData, noteType, isSustainNote) spikeHit(noteData) end
-function opponentNoteHit(membersIndex, noteData, noteType, isSustainNote) spikeHit(noteData) end
-function spikeHit(noteData)
+function goodNoteHit(membersIndex, noteData, noteType, isSustainNote) spikeHit(noteData, isSustainNote) end
+function opponentNoteHit(membersIndex, noteData, noteType, isSustainNote) spikeHit(noteData, isSustainNote) end
+function spikeHit(noteData, isSustainNote)
+	if isSustainNote then return; end
 	local singAnims = {'singLEFT', 'singDOWN', 'singUP', 'singRIGHT'};
 	runHaxeCode([[spike.holdTimer = 0;]])
 	playAnim('spike', singAnims[noteData + 1], true)
@@ -80,7 +179,7 @@ end
 function onEvent(eventName, value1, value2)
 	if eventName == 'Change Character' then
 		if flashingLights then
-			cameraFlash('other', 'FFFFFF', 1.5)
+			cameraFlash('other', 'FFFFFF', 1)
 		end
 	end
 end
